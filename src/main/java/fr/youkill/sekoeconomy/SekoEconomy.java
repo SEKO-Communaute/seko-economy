093d83fe7a72684103df4bf5d7e64149e9f48d83
Julien SERIN <julien.serin@epitech.eu>
2024-05-17 23:42:09 +0200

    [STYLE]: remove useless import

--- src/main/java/fr/youkill/sekoeconomy/SekoEconomy.java
+++ src/main/java/fr/youkill/sekoeconomy/SekoEconomy.java
@@ -1,13 +1,7 @@
 package fr.youkill.sekoeconomy;
 
-import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
-import net.milkbowl.vault.permission.Permission;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
