91edd5721ed27e1061a7c34aefb828e4d78036a7
Julien SERIN <julien.serin@epitech.eu>
2024-05-17 23:41:23 +0200

    [FEAT]: check and get vault economy instance

--- src/main/java/fr/youkill/sekoeconomy/SekoEconomy.java
+++ src/main/java/fr/youkill/sekoeconomy/SekoEconomy.java
@@ -1,16 +1,47 @@
 package fr.youkill.sekoeconomy;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SekoEconomy extends JavaPlugin {
+    private static Economy economy = null;
 
     @Override
     public void onEnable() {
-        System.out.println("Hello from Sekoeconomy");
+        if (!setupEconomy() ) {
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getLogger().info("Plugin loaded");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            getLogger().severe("Vault plugin not found !");
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            getLogger().severe("Vault economy addons not found !");
+            return false;
+        }
+        economy = rsp.getProvider();
+        return true;
+    }
+
+    public static Economy getEconomy() {
+        return economy;
     }
 }
