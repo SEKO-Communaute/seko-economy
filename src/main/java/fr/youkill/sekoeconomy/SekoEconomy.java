a66c179a34cde391c3017693a4b6c7a68dddb2b8
Julien SERIN <julien.serin@epitech.eu>
2024-05-17 20:57:29 +0200

    Initial Commit

---
+++ src/main/java/fr/youkill/sekoeconomy/SekoEconomy.java
@@ -0,0 +1,16 @@
+package fr.youkill.sekoeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SekoEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello from Sekoeconomy");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
